a9da4409563bf035f6d9517a687a4d1d2aebdc7a
Israelvlastuin <124810096+Israelvlastuin@users.noreply.github.com>
2023-09-22 16:35:34 -0300

    Update firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAozeTONtoPsitcRZ6km-_SBZGkvhdHdJ0",
+  authDomain: "teste-aprendeno.firebaseapp.com",
+  projectId: "teste-aprendeno",
+  storageBucket: "teste-aprendeno.appspot.com",
+  messagingSenderId: "522101921979",
+  appId: "1:522101921979:web:7e28d243b8d090795cf19e",
+  measurementId: "G-8ZHXERH9EL"
+  };
+firebase.initializeApp(firebaseConfig);
